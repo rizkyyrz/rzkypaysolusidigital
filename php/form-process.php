989e18d513b43ac7c464b5d88203938072e6a80a
Muhammad Rizky Ramadhan <51981145+rizkyyrz@users.noreply.github.com>
2024-08-02 11:29:29 +0700

    Update form-process.php

--- php/form-process.php
+++ php/form-process.php
@@ -16,41 +16,46 @@ if (empty($_POST["email"])) {
     $email = $_POST["email"];
 }
 
-// MESSAGE
+// PHONE
 if (empty($_POST["phone"])) {
     $errorMSG .= "Phone is required ";
 } else {
     $phone = $_POST["phone"];
 }
 
-
-$EmailTo = "aakram23@gmail.com";
-$Subject = "New Message Received";
-
-// prepare email body text
-$Body = "";
-$Body .= "Name: ";
-$Body .= $name;
-$Body .= "\n";
-$Body .= "Email: ";
-$Body .= $email;
-$Body .= "\n";
-$Body .= "Phone: ";
-$Body .= $phone;
-$Body .= "\n";
-
-// send email
-$success = mail($EmailTo, $Subject, $Body, "From:".$email);
-
-// redirect to success page
-if ($success && $errorMSG == ""){
-   echo "success";
-}else{
-    if($errorMSG == ""){
-        echo "Something went wrong :(";
-    } else {
-        echo $errorMSG;
+if ($errorMSG == "") {
+    // URL tujuan
+    $url = "https://example.com/submit-form";
+    
+    // Data yang akan dikirim
+    $data = [
+        'name' => $name,
+        'email' => $email,
+        'phone' => $phone
+    ];
+    
+    // Inisialisasi cURL
+    $ch = curl_init($url);
+    
+    // Setel opsi cURL
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+    
+    // Eksekusi cURL
+    $response = curl_exec($ch);
+    
+    // Periksa apakah terjadi kesalahan
+    if ($response === false) {
+        $errorMSG = 'Curl error: ' . curl_error($ch);
     }
+    
+    // Tutup cURL
+    curl_close($ch);
+    
+    // Tampilkan respons
+    echo $response;
+} else {
+    echo $errorMSG;
 }
-
 ?>
